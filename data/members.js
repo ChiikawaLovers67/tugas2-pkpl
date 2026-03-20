4b26f2c400abaefed3132a470fed8afd63d8b612
Maira <mairaazsh@gmail.com>
2026-03-20 10:43:16 +0700

    feat: Biodata Page

---
+++ data/members.js
@@ -0,0 +1,44 @@
+const members = [
+  { 
+    name: "Ahmad Faiq Fawwaz Abdussalam", 
+    npm: "2406397706", 
+    image: "https://cosmic.csui.dev/_next/image?url=%2Fimages%2Fahmad-faiq-fawwaz-abdussalam.jpg&w=1920&q=75" ,
+    major: "Ilmu Komputer Reguler",
+    birthdate: "09 Oktober 2006" ,
+    description: "Anak pacil dari lampung yang hobinya nonton f1 sama bola. Lagi berusaha survive di pacil demi ipk >3.6 dan gaji 2 digit setelah lulus"
+  },
+  { 
+    name: "Inayah Saffanah Asri", 
+    npm: "2406353276", 
+    image: "https://cosmic.csui.dev/_next/image?url=%2Fimages%2Finayah-saffanah-asri.jpg&w=1920&q=75" ,
+    major: "Ilmu Komputer Reguler",
+    birthdate: "01 April 2006" ,
+    description: "Mau jadi kucing"
+  },
+  { 
+    name: "Naila Khadijah", 
+    npm: "2406407796", 
+    image: "https://cosmic.csui.dev/_next/image?url=%2Fimages%2Fnaila-khadijah.jpg&w=1920&q=75" ,
+    major: "Ilmu Komputer Reguler",
+    birthdate: "10 Agustus 2006",
+    description: "Nail(a) seorang pacilian yang sedang mencoba menyukai kehidupan pacil." 
+  },
+  { 
+    name: "Muhammad Nadhif Ibrahim", 
+    npm: "2406398324", 
+    image: "https://cosmic.csui.dev/_next/image?url=%2Fimages%2Fmuhammad-nadhif-ibrahim.jpg&w=1920&q=75" ,
+    major: "Ilmu Komputer Reguler",
+    birthdate: "14 Juli 2006",
+    description: "Seorang mahasiswa Ilmu Komputer Universitas Indonesia yang memiliki ketertarikan pada teknologi dan seni. Hobinya adalah membaca dan menonton." 
+  },
+  { 
+    name: "Maira Azma Shaliha", 
+    npm: "2406408086", 
+    image: "https://cosmic.csui.dev/_next/image?url=%2Fimages%2Fmaira-azma-shaliha.jpg&w=1920&q=75" ,
+    major: "Ilmu Komputer Reguler",
+    birthdate: "24 September 2006" ,
+    description: "Suka anime, kucing, dan tidur."
+  }
+]
+
+export default members
